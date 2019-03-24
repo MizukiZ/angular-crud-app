a636a8251e2439eba0b856698a97f46331b6d716
Mizuki Zenta <mizukisb@gmail.com>
2019-03-24 18:18:30 +1100

    implemented all features other than update

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,15 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core"
+import { ActivatedRoute } from "@angular/router"
+import { TodoService } from "./todo.service"
 
 @Component({
-  selector: 'app-todo',
-  templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.scss']
+  selector: "app-todo",
+  templateUrl: "./todo.component.html",
+  styleUrls: ["./todo.component.scss"]
 })
 export class TodoComponent implements OnInit {
+  private todos
+  public activeTasks
+  private newTodo
+  private path
 
-  constructor() { }
+  constructor(
+    private todoService: TodoService,
+    private route: ActivatedRoute
+  ) {}
 
-  ngOnInit() {
+  getTodos(query = "") {
+    return this.todoService.get(query).then(todos => {
+      console.log(`Current todo objects ${JSON.stringify(todos)}`)
+      this.todos = todos
+      this.activeTasks = this.todos.filter(todo => todo.isDone).length
+    })
   }
 
+  addTodo() {
+    this.todoService.add({ title: this.newTodo, isDone: false }).then(() => {
+      this.getTodos(this.path).then(() => {
+        this.newTodo = ""
+      })
+    })
+  }
+
+  statusToggle(todo, e) {
+    status = e.target.checked
+    this.todoService.toggleStatus(todo, status).then(() => {
+      this.getTodos(this.path)
+    })
+  }
+
+  updateTodo(todo, newValue) {
+    todo.title = newValue
+    return this.todoService.put(todo).then(() => {
+      todo.editing = false
+      return this.getTodos(this.path)
+    })
+  }
+
+  destroyTodo(todo) {
+    this.todoService.destroy(todo).then(() => {
+      this.getTodos(this.path)
+    })
+  }
+
+  clearCompleted() {
+    this.todoService.deleteCompleted().then(() => {
+      this.getTodos(this.path)
+    })
+  }
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.path = params["status"]
+      this.getTodos(this.path)
+    })
+  }
 }
---
+++ src/app/todo/todo.service.ts
@@ -0,0 +1,69 @@
+import { Injectable } from "@angular/core"
+import { resolve } from "path"
+
+let TODOS = [
+  { title: "Install Angular CLI", isDone: true },
+  { title: "Style app", isDone: true },
+  { title: "Finish service functionality", isDone: false },
+  { title: "Setup API", isDone: false }
+]
+
+@Injectable({
+  providedIn: "root"
+})
+export class TodoService {
+  constructor() {}
+
+  get(query) {
+    return new Promise(resolve => {
+      let data
+
+      if (query === "active" || query === "completed") {
+        const isCompleted = query === "completed"
+        data = TODOS.filter(todo => todo.isDone == isCompleted)
+      } else {
+        data = TODOS
+      }
+      resolve(data)
+    })
+  }
+
+  add(data) {
+    return new Promise(resolve => {
+      TODOS.push(data)
+      resolve(data)
+    })
+  }
+
+  put(changed) {
+    return new Promise(resolve => {
+      const index = TODOS.findIndex(todo => todo === changed)
+      TODOS[index].title = changed.title
+      resolve(changed)
+    })
+  }
+
+  destroy(destroyed) {
+    return new Promise(resolve => {
+      const index = TODOS.findIndex(todo => todo === destroyed)
+      TODOS.splice(index, 1)
+      resolve(true)
+    })
+  }
+
+  toggleStatus(toggled, status) {
+    return new Promise(resolve => {
+      const index = TODOS.findIndex(todo => todo === toggled)
+      const boolStatus = status === "true" ? true : false
+      TODOS[index].isDone = boolStatus
+      resolve(toggled)
+    })
+  }
+
+  deleteCompleted() {
+    return new Promise(resolve => {
+      TODOS = TODOS.filter(todo => !todo.isDone)
+      resolve(TODOS)
+    })
+  }
+}
